4760c39135d180d8e73e89cfe98ba8cdbaeb8820
Lars Børty Nielsen <lars.borty@gmail.com>
2019-05-14 16:01:16 +0200

    des

--- R/ccmean.R
+++ R/ccmean.R
@@ -6,7 +6,6 @@
 #' - Naive "Available Sample"
 #' - Naive "Complete Case"
 #' 
-#' - LinT - Lin's method
 #' - BT - Bang and Tsiatis's method 
 #' - ZT - Zhao and Tian's method
 #' 
@@ -17,8 +16,9 @@
 #' ccmean(df, id="id", tcost="tcost", delta="delta", surv="surv", L = NA)
 
 
-ccmean <- function(x, id="id", cost="cost", start="start", stop="stop", delta="delta", surv="surv", L = NA, addInterPol = 0) {
-## Set estimation period if undefined
+ccmean <- function(x, id = "id", cost = "cost", start = "start", stop = "stop", delta = "delta", surv = "surv", L = NA, addInterPol = 0) {
+
+# Set estimation period if undefined
 if(is.na(L)) L <- max(x$surv)
 	
 # Subset to estimation period	
@@ -34,13 +34,12 @@ x$stop <- pmin(x$stop, L)
 x <- x[order(x$surv, x$delta),]
 row.names(x) <- 1:nrow(x)
 
-
 # Some calculations don't use cost history and therefore collapse by ID
-  xf <- x %>% 
-    group_by(id) %>% 
-    summarize(cost = sum(cost, na.rm=T),
-              delta = last(delta),
-              surv = first(surv))
+xf <- x %>% 
+  group_by(id) %>% 
+  summarize(cost  = sum(cost, na.rm=T),
+            delta = last(delta),
+            surv  = first(surv))
 
 
 #################################################################
@@ -49,9 +48,15 @@ row.names(x) <- 1:nrow(x)
 #################################################################
 
 # Costs are summed and a mean are found
-available_sample <- mean(xf$cost)
+AS <- mean(xf$cost)
 
-available_sample_full <- c(available_sample, NA, NA, NA, NA)
+AS_var <- var(xf$cost)
+
+AS_full <- c(AS,
+             AS_var,
+             sqrt(AS_var),
+             AS + 1.96 * sqrt(AS_var),
+             AS - 1.96 * sqrt(AS_var))
 
 #################################################################
 ##                          section 2:                         ##
@@ -59,42 +64,48 @@ available_sample_full <- c(available_sample, NA, NA, NA, NA)
 #################################################################
 
 # Costs are summed up and calculated mean
-complete_case <- mean(xf$cost[xf$delta==1])
+CC <- mean(xf$cost[xf$delta==1])
 
-complete_case_full <- c(complete_case, NA, NA, NA, NA)
+CC_var <- var(xf$cost[xf$delta==1])
+
+CC_full <- c(CC,
+             CC_var,
+             sqrt(CC_var),
+             CC + 1.96 * sqrt(CC_var),
+             CC - 1.96 * sqrt(CC_var))
 
 
-#################################################################
-##                          section 4:                         ##
-##                     Lin's method (1997)                     ##
-#################################################################
+# #################################################################
+# ##                          section 4:                         ##
+# ##                     Lin's method (1997)                     ##
+# #################################################################
+# 
+# # Kaplan-meier for censoring distribution get censoring times to define intervals
+# sc <- summary(survfit(Surv(xf$surv, xf$delta == 0) ~ 1))
+# censBreaks <- c(0, sc$time, Inf)
+#   
+# # calculate average costs of patients deceased within each interval
+# a <- subset(xf, delta == 1) %>% 
+#     mutate(ints = cut(surv, breaks = censBreaks, dig.lab = 5)) %>%
+#     group_by(ints) %>% 
+#     summarise(mean = mean(cost))
+#   
+# # Get survival times for intervals
+# sd <- survfit(Surv(xf$surv, xf$delta == 1) ~ 1)
+# intLow <- as.numeric(gsub("\\(", "", sapply(strsplit(as.character(a$ints), ","), function(x) x[[1]])))
+# intHigh <- as.numeric(gsub("\\]", "", sapply(strsplit(as.character(a$ints), ","), function(x) x[[2]])))
+# svLow <- summary(sd, times = intLow)$surv
+# svHigh <- c(summary(sd, times = intHigh)$surv)
+# if(length(svHigh) < length(svLow)){ ## Add zero if last value of intHigh is Inf
+# 	svHigh <- c(svHigh,0)
+# }
+#   
+# # Gathering the data in a new dataframe
+# d <- data.frame(a, "survDif" = svLow-svHigh)
+#     
+# # calculating Lin's T estimate of total costs 
+# LinT <- sum(d$survDif*d$mean, na.rm=T)
 
-# Kaplan-meier for censoring distribution get censoring times to define intervals
-sc <- summary(survfit(Surv(xf$surv, xf$delta == 0) ~ 1))
-censBreaks <- c(0, sc$time, Inf)
-  
-# calculate average costs of patients deceased within each interval
-a <- subset(xf, delta == 1) %>% 
-    mutate(ints = cut(surv, breaks = censBreaks, dig.lab = 5)) %>%
-    group_by(ints) %>% 
-    summarise(mean = mean(cost))
-  
-# Get survival times for intervals
-sd <- survfit(Surv(xf$surv, xf$delta == 1) ~ 1)
-intLow <- as.numeric(gsub("\\(", "", sapply(strsplit(as.character(a$ints), ","), function(x) x[[1]])))
-intHigh <- as.numeric(gsub("\\]", "", sapply(strsplit(as.character(a$ints), ","), function(x) x[[2]])))
-svLow <- summary(sd, times = intLow)$surv
-svHigh <- c(summary(sd, times = intHigh)$surv)
-if(length(svHigh) < length(svLow)){ ## Add zero if last value of intHigh is Inf
-	svHigh <- c(svHigh,0)
-}
-  
-# Gathering the data in a new dataframe
-d <- data.frame(a, "survDif" = svLow-svHigh)
-    
-# calculating Lin's T estimate of total costs 
-LinT <- sum(d$survDif*d$mean, na.rm=T)
-LinT_full <- c(LinT, NA, NA, NA, NA)
 
 #################################################################
 ##                          section 5:                         ##
@@ -115,16 +126,18 @@ st <- data.frame(s$time, s$surv)
 st <- unique(st)
 
 # Merge probalities of censoring and survival to data
-t <- merge(xf,sct,by.x="surv", by.y="sc.time", all.x=T)
-t <- merge(t,st,by.x="surv", by.y="s.time", all.x=T)
+t <- merge(xf, sct, by.x = "surv", by.y = "sc.time", all.x = T)
+t <- merge(t, st, by.x = "surv", by.y = "s.time", all.x = T)
 
 # Calculate Bang and Tsiatis cost estimator
 BT <- mean((t$cost*t$delta)/t$sc.surv)
 
 # START VAR BT ------------------------------------------------------------
+
 n <- length(t$cost)
 t$GA <- rep(0, n)
 t$GB <- rep(0, n)
+
 for(i in 1:n){
   if(t$delta[i] == 1) next
   t2 <- subset(t, surv >= t$surv[i])
@@ -132,13 +145,16 @@ for(i in 1:n){
   t$GB[i] <- (1 / (n*t$s.surv[i])) * sum(t2$delta * t2$cost / t2$sc.surv)
 }
 
-BT_var <- 1/n * (mean(t$delta*(t$cost-BT)^2/t$sc.surv) + 
-                   mean(((1-t$delta)/t$sc.surv^2 )* (t$GA - t$GB^2)))
-BT_sd <- sqrt(BT_var)
-BT_uci <- BT + (1.96 * BT_sd)
-BT_lci <- BT - (1.96 * BT_sd)
+BT_var <- 1 / n * (mean(t$delta * (t$cost-BT)^2 / t$sc.surv) + 
+                     mean(((1 - t$delta) / t$sc.surv^2 ) * (t$GA - t$GB^2)))
 
-BT_full <- c(BT, BT_var, BT_sd, BT_uci, BT_lci)
+
+BT_full <- c(BT,
+             BT_var,
+             sqrt(BT_var),
+             BT + 1.96 * sqrt(BT_var),
+             BT - 1.96 * sqrt(BT_var))
+
 
 # END VAR BT --------------------------------------------------------------
 
@@ -194,40 +210,41 @@ for(i in 1:n){
    t$gmm[i] <- (1/(n*t$s.surv[i])) * sum(as.numeric(t$surv >= t$surv[i]) * t$delta * t$cost * runCostMatrix[,i] / t$sc.surv)
 }
   
-ztVAR <- BT_var - (2/n^2) * sum(((1-t$delta) /  t$sc.surv^2) * (t$gmm - t$GB * t$gm)) + 
+ZT_var <- BT_var - (2/n^2) * sum(((1-t$delta) /  t$sc.surv^2) * (t$gmm - t$GB * t$gm)) + 
   (1/n^2) * sum(((1-t$delta) /  t$sc.surv^2) * (t$mcostlsurvSq - t$mcostlsurv^2))
 
 
 ZT_full <- c(ZT,
-             ztVAR,
-             sqrt(ztVAR),
-             ZT + 1.96 * sqrt(ztVAR),
-             ZT - 1.96 * sqrt(ztVAR))
+             ZT_var,
+             sqrt(ZT_var),
+             ZT + 1.96 * sqrt(ZT_var),
+             ZT - 1.96 * sqrt(ZT_var))
 
 #################################################################
 ##                          section 7:                         ##
 ##                           Results                           ##
 #################################################################
 
-results <- list("These results should be checked before ...",
-                round(data.frame(available_sample, 
-                           complete_case, 
-                           LinT, 
-                           BT,
-                           ZT),2),
-                round(data.frame(available_sample_full,
-                           complete_case_full,
-                           LinT_full,
-                           BT_full,
-                           ZT_full, 
-                           row.names = c("Estimate", "Variance", "SD", "95UCI", "95LCI")),2)
+results <- list(Text  = c("CCOSTR - Estimation of censored costs"),
+                Data  = data.frame(observations = nrow(x), 
+                                   induviduals  = nrow(xf), 
+                                   events       = sum(xf$delta==1), 
+                                   row.names    = "n"),
+                First = data.frame(AS, CC, BT, ZT),
+                Estimates = round(data.frame("AvailableSample" = AS_full,
+                                             "CompleteCase"    = CC_full,
+                                             "BT"              = BT_full,
+                                             "ZT"              = ZT_full, 
+                                             row.names         = c("Estimate", "Variance", "SD", "95UCI", "95LCI")),2)
                 )
-return(results)
+
+results[c(1,2,4)]
+
 }
 
 
-
-
+ccmean(df_1)
+ccmean(df_2)
 
 
 
