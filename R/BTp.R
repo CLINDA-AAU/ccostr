a9ae4690b52a41a2f158c1cf03088d3d815d2ff4
Lars Børty Nielsen <lars.borty@gmail.com>
2019-09-09 11:38:20 +0200

    rewrote BTp to data.table

--- R/BTp.R
+++ R/BTp.R
@@ -2,37 +2,37 @@
 #' @details Not ready for use... still experimental
 #' 
 #' @param x A dataframe with columns: id, cost, delta and surv. If Cost history is available it can be specified by: start and stop,
-#' @param n number of observations
+#' @param L limit of time
 #' 
 #' @return An score.
 #' 
 #' @examples
-#' BTp(simCostData(100)$censoredCostHistory, n=100)
+#' BTp(simCostData(100)$censoredCostHistory, L = 10)
 #' 
 #' 
-#' @importFrom Rdpack reprompt
-#' @importFrom rlang .data
-#' @import dplyr survival knitr tibble
+#' @importFrom data.table as.data.table := data.table first
+#' @import survival tibble
 
-BTp <- function(x) {
+BTp <- function(x, L = 10) {
   # BTp
-  xf  <- x %>% 
-    group_by(.data$id) %>% 
-    summarize(delta = first(.data$delta),
-              surv  = first(.data$surv))
-  scf <- summary(survfit(Surv(xf$surv, xf$delta == 0) ~ 1), times = c(1:10, xf$surv))
-  scf <- data.frame(scf$time, scf$surv)
+  x  <- as.data.table(x)
+  xf <- x[,.(delta = data.table::first(delta), 
+             surv = data.table::first(surv)), 
+          by = id]
+  scf <- summary(survfit(Surv(xf$surv, xf$delta == 0) ~ 1), times = c(1:L, xf$surv))
+  scf <- data.table(surv = scf$time, scf.surv = scf$surv)
   
   BTp <- NULL
-  for (i in 1:10) {
-    sx         <- subset(x, x$start == i - 1)
-    sx$delta   <- ifelse(sx$stop == i, 1, sx$delta)
-    sx$surv    <- pmin(sx$stop, sx$surv)
-    sx         <- left_join(sx, scf, by = c("surv" = "scf.time"))
-    BTp[i]     <- sum((sx$cost * sx$delta) / sx$scf.surv)
+  for (i in 1:L) {
+    sx <- x[start == i - 1]
+    sx[,                delta := ifelse(stop == i, 1, delta)]
+    sx[,                 surv := pmin(stop, surv)]
+    sx[scf, on = "surv", surv := i.scf.surv]
+    
+    BTp[i] <- sum((sx$cost * sx$delta) / sx$surv, na.rm = TRUE)
   }
   
-  estimate <- sum(BTp)/nrow(xf)
+  estimate <- sum(BTp) / nrow(xf)
   estimate
 }
 
